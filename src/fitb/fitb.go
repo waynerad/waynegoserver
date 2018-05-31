14eb3b931859cc7fc05e52f1b8980ddb04ca4e7e
Wayne Radinsky <waynerad@gmail.com>
2018-05-31 16:41:35 -0600

    Moved timeIntervalSecondsToEnglish to my new utilities package, wutil

--- src/fitb/fitb.go
+++ src/fitb/fitb.go
@@ -15,6 +15,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"wutil"
 )
 
 func getDoctype() string {
@@ -2589,48 +2590,6 @@ func updateInterval(db mysql.Conn, questionjctid uint64, askTime uint64, timeInt
 	_, _, err = stmt.Exec()
 }
 
-// show time interval, e.g. "5 sec", "14 min", etc
-func timeIntervalSecondsToEnglish(inv int) string {
-	if inv < 0 {
-		return "0 seconds"
-	}
-	increments := [5]int{60, 60, 24, 7, 52}
-	singulars := [6]string{"second", "minute", "hour", "day", "week", "year"}
-	plurals := [6]string{"seconds", "minutes", "hours", "days", "weeks", "years"}
-	var breakdown [7]int
-	var num int
-	for i, x := range increments {
-		num = inv / x // has to be integer division
-		rem := inv - (num * x)
-		breakdown[i] = rem
-		inv = num
-	}
-	breakdown[6] = num
-	toUse := 2
-	isLeading := true
-	rv := ""
-	for i := 5; i >= 0; i-- {
-		if isLeading {
-			if breakdown[i] != 0 {
-				isLeading = false
-			}
-		}
-		if !isLeading {
-			if toUse != 0 {
-				if breakdown[i] == 1 {
-					rv = rv + ", 1 " + singulars[i]
-				} else {
-					if breakdown[i] != 0 {
-						rv = rv + ", " + intToStr(breakdown[i]) + " " + plurals[i]
-					}
-				}
-				toUse--
-			}
-		}
-	}
-	return rv[1:]
-}
-
 func getCountFromSQLForUserTopicAndTime(db mysql.Conn, sql string, userid uint64, topicid uint64, currentTime uint64, bindCurrentTime bool) int {
 	sel, err := db.Prepare(sql)
 	if err != nil {
@@ -2887,7 +2846,7 @@ func showAskQuestionPage(w http.ResponseWriter, r *http.Request, op string, user
 		title = topicName
 		if takeABreakMode {
 			timeInterval := int(askTimeGmt - currentTime)
-			inEnglish := timeIntervalSecondsToEnglish(timeInterval)
+			inEnglish := wutil.TimeIntervalSecondsToEnglish(timeInterval)
 			fmt.Fprint(w, `<title>`+title+`</title>
 `+getStyle()+`
 </head>
