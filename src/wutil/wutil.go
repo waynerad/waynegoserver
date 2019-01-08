82fbc8bb6ddc6c00a5bed2b337cbad8c8f7d3c46
Wayne Radinsky <waynerad@gmail.com>
2019-01-07 20:41:43 -0700

    Added Linkize() which turns links into clickable links. Added Markdownize() which turns markdown into HTML. Does not handle wiki hypertext links, which have to be handled in the wiki package, not here!

--- src/wutil/wutil.go
+++ src/wutil/wutil.go
@@ -2,6 +2,7 @@ package wutil
 
 import (
 	"strconv"
+	"strings"
 )
 
 func intToStr(ii int) string {
@@ -48,4 +49,218 @@ func TimeIntervalSecondsToEnglish(inv int) string {
 		}
 	}
 	return rv[1:]
+}
+
+func Trim(z string) string {
+	return strings.Trim(z, " \r\n\t")
+}
+
+func FindInString(stg string, substr string, offset int) int {
+	idx := strings.Index(stg, substr)
+	if offset < 0 {
+		return idx
+	}
+	if idx >= offset {
+		return idx
+	}
+	part2 := stg[offset:]
+	idx = strings.Index(part2, substr)
+	if idx < 0 {
+		return idx
+	}
+	return offset + idx
+}
+
+// based on:
+// https://perishablepress.com/stop-using-unsafe-characters-in-urls/
+// which in turn is based on the RFCs:
+// https://www.ietf.org/rfc/rfc3986.txt
+// https://www.ietf.org/rfc/rfc1738.txt
+func ValidURLCharacter(ch byte) bool {
+	// numbers are allowed
+	if (ch >= 48) && (ch <= 57) {
+		return true
+	}
+	// uppercase letters are allowed
+	if (ch >= 65) && (ch <= 90) {
+		return true
+	}
+	// lowercase letters are allowed
+	if (ch >= 97) && (ch <= 122) {
+		return true
+	}
+	// the following characters are allowed: # 35 $ 36 % 37 & 38 ' 39 ( 40 ) 41 * 42 + 43 , 44 - 45 . 46 / 47
+	if (ch >= 35) && (ch <= 47) {
+		return true
+	}
+	// the following characters are allowed: ! 33 : 58 ; 59 = 61 ? 63 @ 64 _ 95
+	if (ch == 33) || (ch == 58) || (ch == 59) || (ch == 61) || (ch == 63) || (ch == 64) || (ch == 95) {
+		return true
+	}
+	return false
+}
+
+func FindEndOfLink(content string, idx int) int {
+	lx := len(content)
+	for {
+		if idx >= lx {
+			return idx
+		}
+		ch := content[idx]
+		if !ValidURLCharacter(ch) {
+			return idx
+		}
+		idx++
+	}
+}
+
+func ReplaceLink(content string, idx int) (string, int) {
+	eolnk := FindEndOfLink(content, idx)
+	linkURL := content[idx:eolnk]
+	replacement := `<a href="` + linkURL + `">` + linkURL + "</a>"
+	additional := len(replacement) - len(linkURL)
+	return content[:idx] + replacement + content[eolnk:], eolnk + additional
+}
+
+func Linkize(content string) string {
+	idx := FindInString(content, "http://", -1)
+	for idx >= 0 {
+		content, idx = ReplaceLink(content, idx)
+		idx = FindInString(content, "http://", idx)
+	}
+	idx = FindInString(content, "https://", -1)
+	for idx >= 0 {
+		content, idx = ReplaceLink(content, idx)
+		idx = FindInString(content, "https://", idx)
+	}
+	return content
+}
+
+type lineProcessing struct {
+	content         string
+	idx             int
+	beginningOfLine int
+	endOfLine       int
+	inBulletList    bool
+	inNumberedList  bool
+	result          string
+}
+
+func ProcessLine(ch byte, lp *lineProcessing) {
+	if ch == 13 {
+		lp.endOfLine = lp.idx
+		inLine := lp.content[lp.beginningOfLine:lp.endOfLine]
+		if inLine == "" {
+			if lp.inBulletList {
+				lp.result += `</ul>
+`
+				lp.inBulletList = false
+			}
+			if lp.inNumberedList {
+				lp.result += `</ol>
+`
+				lp.inNumberedList = false
+			}
+			lp.result += `</p>
+<p>`
+		} else {
+			switch inLine[0] {
+			case 124:
+				// pipe indicates headers
+				if lp.inBulletList {
+					lp.result += `</ul>
+`
+					lp.inBulletList = false
+				}
+				if lp.inNumberedList {
+					lp.result += `</ol>
+`
+					lp.inNumberedList = false
+				}
+				lp.result += "<h2>" + Trim(inLine[1:]) + `</h2>
+`
+			case 42:
+				// asterisk indicates bullet list
+				if lp.inNumberedList {
+					lp.result += "</ol>"
+					lp.inNumberedList = false
+				}
+				if !lp.inBulletList {
+					lp.inBulletList = true
+					lp.result += `<ul>
+`
+				}
+				lp.result += "<li>" + Trim(inLine[1:]) + `</li>
+`
+			case 35:
+				// hash indicates numbered list
+				if !lp.inNumberedList {
+					lp.inNumberedList = true
+					lp.result += `
+<ol>
+`
+				}
+				lp.result += "<li>" + Trim(inLine[1:]) + `</li>
+`
+			default:
+				if lp.inBulletList {
+					lp.result += `</ul>
+`
+					lp.inBulletList = false
+				}
+				if lp.inNumberedList {
+					lp.result += `
+</ol>
+`
+					lp.inNumberedList = false
+				}
+				lp.result += inLine + `
+`
+			}
+		}
+		lp.beginningOfLine = -1
+	}
+	if ch == 10 {
+		lp.beginningOfLine = lp.idx + 1
+	}
+}
+
+func Markdownize(content string) string {
+	// line-based processing
+	var lp lineProcessing
+	lp.content = content
+	lp.result = "<p>"
+	lp.idx = 0
+	lp.beginningOfLine = 0
+	lp.endOfLine = 0
+	lp.inBulletList = false
+	lp.inNumberedList = false
+	lx := len(content)
+	for lp.idx < lx {
+		ch := content[lp.idx]
+		ProcessLine(ch, &lp)
+		lp.idx++
+	}
+	// we add a "virtual CR" to the end to process the last line
+	// finalize everything
+	if lp.beginningOfLine >= 0 {
+		ch := byte(13)
+		ProcessLine(ch, &lp)
+		if lp.inBulletList {
+			lp.result += `</ul>
+`
+		}
+		if lp.inNumberedList {
+			lp.result += `</ol>
+`
+		}
+	}
+	lp.result += "</p>"
+	// linkize
+	idxh := strings.Index(lp.result, "http://")
+	idxhs := strings.Index(lp.result, "https://")
+	if (idxh >= 0) || (idxhs >= 0) {
+		lp.result = Linkize(lp.result)
+	}
+	return lp.result
 }
